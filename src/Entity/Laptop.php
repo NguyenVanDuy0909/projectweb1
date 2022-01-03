79a795d9cc41898e8dfb95461a907fc550964e22
NguyenVanDuy0909 <80749076+NguyenVanDuy0909@users.noreply.github.com>
2022-01-03 14:35:51 +0700

    set entity

---
+++ src/Entity/Laptop.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LaptopRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LaptopRepository::class)]
+class Laptop
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $madein;
+
+    #[ORM\Column(type: 'integer')]
+    private $price;
+
+    #[ORM\Column(type: 'integer')]
+    private $PriceDiscount;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMadein(): ?string
+    {
+        return $this->madein;
+    }
+
+    public function setMadein(string $madein): self
+    {
+        $this->madein = $madein;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getPriceDiscount(): ?int
+    {
+        return $this->PriceDiscount;
+    }
+
+    public function setPriceDiscount(int $PriceDiscount): self
+    {
+        $this->PriceDiscount = $PriceDiscount;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
